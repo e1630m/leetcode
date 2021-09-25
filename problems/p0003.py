66f25b9184d9cb6e4e2bd01f316ecc7bb2031e71
e1630m <e1630m@gmail.com>
2021-09-25 09:16:05 +0000

    Update problems/p0003.py

--- problems/p0003.py
+++ problems/p0003.py
@@ -1,3 +1,8 @@
+# 3. Longest Substring Without Repeating Characters
+# https://leetcode.com/problems/longest-substring-without-repeating-characters
+# 987 / 987 test cases passed
+# runtime: 60ms, beats 79.49% of python3 submissions
+# memory usage: 14.2mb, beats 79.34% of python3 submissions
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         start = longest = 0
