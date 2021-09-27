6389f34f6bf001a01498454d21f3ea6c178d8ccc
e1630m <e1630m@gmail.com>
2021-09-27 11:20:55 +0000

    Update p1137.py (comments removed)

--- problems/p1137.py
+++ problems/p1137.py
@@ -1,8 +1,3 @@
-# 1137. N-th Tribonacci Number
-# https://leetcode.com/explore/item/3986
-# 39 / 39 test cases passed.
-# runtime: 24ms, beats 95.40% of python3 submissions.
-# memory usage: 14.3mb, beats 41.36% of python3 submissions
 class Solution:
     def tribonacci(self, n: int) -> int:
         if n == 0:
@@ -11,4 +6,3 @@ class Solution:
         for _ in range(2, n):
             a, b, c = b, c, a + b + c
         return c
-
