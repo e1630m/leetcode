da942a6886345e93e7ee6f43f3f68ecaae6aa2b0
e1630m <e1630m@gmail.com>
2021-09-27 11:20:42 +0000

    Update p0004.py (comments removed)

--- problems/p0004.py
+++ problems/p0004.py
@@ -1,8 +1,3 @@
-# 4. Median of Two Sorted Arrays
-# https://leetcode.com/problems/median-of-two-sorted-arrays
-# 2094 / 2094 test cases passed
-# runtime: 80ms, beats 98.27% of python3 submissions
-# memory usage: 14.5mb, beats 53.70% of python3 submissions
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
         l = sorted(nums2 + nums1)
