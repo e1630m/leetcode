57264f1fb5cbb68f00749acf490cb12b39371f76
e1630m <e1630m@gmail.com>
2021-09-27 11:20:25 +0000

    Update p0001.py (comments removed)

--- problems/p0001.py
+++ problems/p0001.py
@@ -1,8 +1,3 @@
-# 1. Two Sum
-# https://leetcode.com/problems/two-sum
-# 55 / 55 test cases passed
-# runtime: 432ms, beats 37.54% of python3 submissions
-# memory usage: 15.1mb, beats 63.29% of python3 submissions
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         sub = []
@@ -10,4 +5,3 @@ class Solution:
             if n in sub:
                 return [nums.index(target - n), i]
             sub.append(target - n)
-
